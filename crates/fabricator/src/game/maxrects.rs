8df4201b6c3268533700b352c95460954b4dd7a3
Gabe Weiner <imlazyeye@gmail.com>
2026-05-13 10:09:03 -0400

    fix: update test to use new rng api

--- crates/fabricator/src/game/maxrects.rs
+++ crates/fabricator/src/game/maxrects.rs
@@ -155,7 +155,7 @@ struct FitScore<N> {
 mod tests {
     use std::{collections::HashMap, ops::Add};
 
-    use rand::{Rng, SeedableRng, rngs::SmallRng};
+    use rand::{RngExt, SeedableRng, rngs::SmallRng};
 
     use super::*;
 
